a6eaf230cae8e0695c3c9ce7034d42c28cd0edd3
@Mikist <mikeochango3@gmail.com>
2024-04-02 21:07:13 +0300

    Created posts api routes

--- api/app.js
+++ api/app.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import authRoute from "./routes/auth.route.js";
-//import postRoute from "./routes/post.route.js";
+import postRoute from "./routes/post.route.js";
 import testRoute from "./routes/test.route.js";
 import userRoute from "./routes/user.route.js";
 //import chatRoute from "./routes/chat.route.js";
@@ -19,7 +19,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
-//app.use("/api/posts", postRoute);
+app.use("/api/posts", postRoute);
 app.use("/api/test", testRoute);
 //app.use("/api/chats", chatRoute);
 //app.use("/api/messages", messageRoute);
