294361870ea81880f1c7daf0a5ac0ee66c66c1db
@Mikist <mikeochango3@gmail.com>
2024-04-02 11:56:34 +0300

    Performing crud opperation on users

--- api/app.js
+++ api/app.js
@@ -6,8 +6,8 @@ dotenv.config();
 
 import authRoute from "./routes/auth.route.js";
 //import postRoute from "./routes/post.route.js";
-//import testRoute from "./routes/test.route.js";
-//import userRoute from "./routes/user.route.js";
+import testRoute from "./routes/test.route.js";
+import userRoute from "./routes/user.route.js";
 //import chatRoute from "./routes/chat.route.js";
 //import messageRoute from "./routes/message.route.js";
 
@@ -18,9 +18,9 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoute);
-//app.use("/api/users", userRoute);
+app.use("/api/users", userRoute);
 //app.use("/api/posts", postRoute);
-//app.use("/api/test", testRoute);
+app.use("/api/test", testRoute);
 //app.use("/api/chats", chatRoute);
 //app.use("/api/messages", messageRoute);
 
