025ffb994519a6fe7c46808bdf5c52829c17c112
@Mikist <mikeochango3@gmail.com>
2024-09-10 23:53:41 +0300

    update the imports

--- client/src/routes/listPage/listPage.jsx
+++ client/src/routes/listPage/listPage.jsx
@@ -5,6 +5,7 @@ import Map from "../../components/map/Map";
 import { Await, useLoaderData } from "react-router-dom";
 import { Suspense } from "react";
 
+
 function ListPage() {
   const data = useLoaderData();
 
