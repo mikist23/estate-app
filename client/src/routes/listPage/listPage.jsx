263801e7f446420f8e1d22414aa528d9889bbc2a
@Mikist <mikeochango3@gmail.com>
2024-09-11 23:34:52 +0300

    update import  filex

--- client/src/routes/listPage/listPage.jsx
+++ client/src/routes/listPage/listPage.jsx
@@ -2,6 +2,7 @@ import "./listPage.scss";
 import Filter from "../../components/filter/Filter";
 import Card from "../../components/card/Card";
 import Map from "../../components/map/Map";
+
 import { Await, useLoaderData } from "react-router-dom";
 import Cards from "../../components/card/Card";
 import { Suspense } from "react";
